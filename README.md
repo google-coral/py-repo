504334b2a8b18a2243d00f256871989e9b4db600
Dmitry Kovalev <dkovalev@google.com>
2021-01-15 13:55:26 -0800

    Add PEP 503 repo generator

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # py-repo
+
+```shell
+python3 generate_repo.py --base_url=https://github.com/google-coral/pycoral/releases/download/release-frogfish pycoral@1.0.0 tflite-runtime@2.5.0
+```
---
+++ generate_repo.py
@@ -0,0 +1,106 @@
+# Lint as: python3
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import argparse
+import hashlib
+import os
+import sys
+import urllib.request
+
+
+def get_sha256(url):
+  response = urllib.request.urlopen(url)
+  return hashlib.sha256(response.read()).hexdigest()
+
+
+def index_html(packages):
+  content = '\n      '.join(
+      f'<li><a href="{name}">{name}</a></li>' for name, version in packages)
+  return f"""<!DOCTYPE html>
+<html>
+  <head><title>Coral PEP-503 Repository</title></head>
+  <body>
+    <ul>
+      {content}
+    </ul>
+  </body>
+</html>
+"""
+
+
+def package_index_html(package, base_url):
+  name, version = package
+
+  links = []
+  for py_version in ['cp35-cp35m', 'cp36-cp36m', 'cp37-cp37m', 'cp38-cp38']:
+    for os_arch in ['linux_aarch64', 'linux_armv7l', 'linux_x86_64',
+                    'win_amd64', 'macosx_10_15_x86_64']:
+      escaped_name = name.replace('-', '_')
+      wheel_name = f'{escaped_name}-{version}-{py_version}-{os_arch}.whl'
+      url = f'{base_url}/{wheel_name}'
+      print('Processing: %s' % url)
+      sha256 = get_sha256(url)
+      print('  SHA256=%s' % sha256)
+      links.append(f'<li><a href="{url}#sha256={sha256}">{wheel_name}</a></li>')
+
+  content = '\n      '.join(links)
+  return f"""<!DOCTYPE html>
+<html>
+  <head>
+    <title>{name} wheels</title>
+  </head>
+  <body>
+    <ul>
+      {content}
+    </ul>
+  </body>
+</html>
+"""
+
+
+def generate(output_dir, packages, base_url):
+  os.mkdir(output_dir)
+  with open(os.path.join(output_dir, 'index.html'), 'w') as f:
+    f.write(index_html(packages))
+
+  for package in packages:
+    package_dir = os.path.join(output_dir, package[0])
+    os.mkdir(package_dir)
+    with open(os.path.join(package_dir, 'index.html'), 'w') as f:
+      f.write(package_index_html(package, base_url))
+
+
+def main():
+  parser = argparse.ArgumentParser(description='PEP 503 repo generator.')
+  parser.add_argument('--base_url', required=True,
+                      help='base URL to download wheels')
+  parser.add_argument('--output_dir', default='out',
+                      help='output directory to save repo content')
+  parser.add_argument('packages', metavar='N', type=str, nargs='+',
+                      help='list of <package_name>@<package_version> strings')
+  args = parser.parse_args()
+
+  base_url = args.base_url
+  packages = [tuple(package.split('@')) for package in args.packages]
+  output_dir = args.output_dir
+
+  if os.path.exists(output_dir):
+    print('Output directory "%s" already exists.' % output_dir, file=sys.stderr)
+    return 1
+
+  generate(output_dir, packages, base_url)
+  return 0
+
+if __name__ == '__main__':
+  sys.exit(main())
